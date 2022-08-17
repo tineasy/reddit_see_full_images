8da382c2c3b033e0558211838ab8f30764f1d0f5
Valentin <valentin.stets@icloud.com>
2022-08-17 22:01:08 +0200

    just a new logo and new gulpfile for easier build might keep developing the extension, so...

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const del = require("del");
 const uglify = require("gulp-uglify-es").default;
+const zip = require("gulp-zip");
 
 function cleanDist() {
   return del("dist");
 }
 
 function scripts() {
-  return src(["src/js/**/*.js"], { base: "src" }).pipe(uglify()).pipe(dest("dist"));
+  return src(["src/js/**/*.js"], { base: "src" })
+    .pipe(uglify())
+    .pipe(dest("dist"));
 }
 
 function styles() {
@@ -25,11 +28,20 @@ function styles() {
 }
 
 function build() {
-  return src(["src/icons/**/*", "src/manifest.json"], { base: "src" }).pipe(dest("dist"));
+  return src(["src/icons/**/*", "src/manifest.json"], { base: "src" }).pipe(
+    dest("dist")
+  );
+}
+
+function pack() {
+  return src(["dist/**/*"], { base: "dist" })
+    .pipe(zip("extension.zip"))
+    .pipe(dest("."));
 }
 
 exports.cleanDist = cleanDist;
 exports.scripts = scripts;
 exports.styles = styles;
+exports.pack = pack;
 
-exports.build = series(cleanDist, build, styles, scripts);
+exports.build = series(cleanDist, build, styles, scripts, pack, cleanDist);
